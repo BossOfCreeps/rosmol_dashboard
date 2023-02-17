99fe531b3c149cbf21b5b727e0ff34df1803f121
Rachis Vsevolod <seva-ra4is@mail.ru>
2023-02-17 23:52:01 +0700

    add corsheaders

--- rosmol_dashboard/settings.py
+++ rosmol_dashboard/settings.py
@@ -46,9 +46,11 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework.authtoken",
     'drf_yasg',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -130,3 +132,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOW_ALL_ORIGINS = True
